b722d6e7f798baf653a83a6dc9ad21553f06bfee
Gustavo <gustavomunozespinoza@hotmail.com>
2021-10-19 12:56:37 -0300

    Commands

---
+++ Assets/Patterns/01-Command/Scripts/Commands/FireGunCommand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireGunCommand : MonoBehaviour, ICommand
+{
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Patterns/01-Command/Scripts/Commands/JumpCommand.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpCommand : MonoBehaviour, ICommand
+{
+    public void Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+}
